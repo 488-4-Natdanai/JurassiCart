538fe1af6860a5430944f56787e410a575b1a7ff
Natdanai <natdanai.sir@kkumail.com>
2026-03-12 09:33:48 +0700

    a

---
+++ t.py
@@ -0,0 +1,18 @@
+import sys
+import os
+from PySide6.QtWidgets import QApplication, QLabel
+from PySide6.QtGui import QFontDatabase, QFont
+
+app = QApplication(sys.argv)
+
+dir = os.path.dirname(__file__)
+font_path = os.path.join(dir, "DinopiaRegular-mLrO9.otf")
+
+font_id = QFontDatabase.addApplicationFont(font_path)
+
+print("font_id =", font_id)
+
+label = QLabel("JurassiCart")
+label.show()
+
+sys.exit(app.exec())
